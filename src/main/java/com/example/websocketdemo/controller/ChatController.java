d8f031d3e877ed789f322fad6cdde8429f75b4c9
vicky1998 <vickyyaadav1998@gmail.com>
2019-02-23 13:59:10 +0530

    Initial commit

---
+++ src/main/java/com/example/websocketdemo/controller/ChatController.java
@@ -0,0 +1,42 @@
+package com.example.websocketdemo.controller;
+
+import com.example.websocketdemo.model.ChatMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by rajeevkumarsingh on 24/07/17.
+ */
+@Controller
+public class ChatController {
+    private static final Logger logger = LoggerFactory.getLogger(ChatController.class);
+
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+        logger.info("Got new Message" + chatMessage.toString());
+        return chatMessage;
+    }
+
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    public ChatMessage addUser(@Payload ChatMessage chatMessage,
+                               SimpMessageHeaderAccessor headerAccessor) {
+        // Add username in web socket session
+
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        logger.info("Added new User to chat" + chatMessage.toString());
+        return chatMessage;
+    }
+    public ChatMessage sendkeys(@Payload ChatMessage chatMessage) {
+        logger.info("Got new Message" + chatMessage.toString());
+        return chatMessage;
+    }
+
+
+}
